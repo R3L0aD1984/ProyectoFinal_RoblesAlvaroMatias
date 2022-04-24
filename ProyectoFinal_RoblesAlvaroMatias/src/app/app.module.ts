6b34b9316748c276904a8b12d2043d08258739bd
R3L0aD1984 <95628928+R3L0aD1984@users.noreply.github.com>
2022-04-24 00:24:56 -0300

    Siguiendo con el Header

--- ProyectoFinal_RoblesAlvaroMatias/src/app/app.module.ts
+++ ProyectoFinal_RoblesAlvaroMatias/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { RedessocialesComponent } from './components/redessociales/redessociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgPComponent
+    LogoArgPComponent,
+    RedessocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
