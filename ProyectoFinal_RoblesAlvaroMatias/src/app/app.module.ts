5f787f8379653b2b7fe47c724ca4f623f64f8d7d
R3L0aD1984 <95628928+R3L0aD1984@users.noreply.github.com>
2022-05-04 23:24:30 -0300

    Acomodando componentes

--- ProyectoFinal_RoblesAlvaroMatias/src/app/app.module.ts
+++ ProyectoFinal_RoblesAlvaroMatias/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
-import { RedessocialesComponent } from './components/redessociales/redessociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { LaboralComponent } from './components/laboral/laboral.component';
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgPComponent,
-    RedessocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     LaboralComponent,
