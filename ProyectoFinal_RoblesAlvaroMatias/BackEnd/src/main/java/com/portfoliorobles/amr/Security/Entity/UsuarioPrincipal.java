52467c877b18716db5a3cf7b7324bfc2ea10c04c
R3L0aD1984 <95628928+R3L0aD1984@users.noreply.github.com>
2022-07-20 11:03:06 -0300

    Update UsuarioPrincipal.java

--- ProyectoFinal_RoblesAlvaroMatias/BackEnd/src/main/java/com/portfoliorobles/amr/Security/Entity/UsuarioPrincipal.java
+++ ProyectoFinal_RoblesAlvaroMatias/BackEnd/src/main/java/com/portfoliorobles/amr/Security/Entity/UsuarioPrincipal.java
@@ -1,7 +1,10 @@
 package com.portfoliorobles.amr.Security.Entity;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 /**
  * @author Matias Robles
@@ -12,4 +15,19 @@ public class UsuarioPrincipal implements UserDetails{
     private String email;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
+    
+    //Constructores
+
+    public UsuarioPrincipal(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    
+    public static UsuarioPrincipal build(Usuario usuario){
+        List<GrantedAuthority> authorities = usuario.getRoles().stream().map(rol -> new SimpleGrantedAuthority(rol.getRolNombre().name()) ).collect(Collectors.toList());
+        return new UsuarioPrincipal(usuario.getNombre(), usuario.getNombreUsuario(), usuario.getEmail(), usuario.getPassword(), authorities);
+    }
 }
